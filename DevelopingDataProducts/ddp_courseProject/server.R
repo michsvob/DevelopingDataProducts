4448b8de48967775f84b167fda6282330884d367
michsvob <svoboda.michal@seznam.cz>
2016-12-06 21:34:16 +0100

    initial commit

---
+++ DevelopingDataProducts/ddp_courseProject/server.R
@@ -0,0 +1,68 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+library(openxlsx)
+library(dplyr)
+library(lubridate)
+
+setwd("/home/michal/DevelopingDataProducts/ddp_courseProject")
+
+translSeverity <- function(y) {
+  sapply(y, function(x)
+    switch(x, "1" = "Fatal", "2" = "Serious", "3" = "Slight"))
+}
+translWeekday <- function(y) {
+  sapply(y, function(x)
+    switch(
+      x,
+      "1" = "Sunday",
+      "2" = "Monday",
+      "3" = "Tuesday",
+      "4" = "Wednesday",
+      "5" = "Thursday",
+      "6" = "Friday",
+      "7"= "Saturday"
+    ))
+}
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  ds <- read.csv("RoadSafetyData_2015/Accidents_2015.csv") %>%
+    select(Longitude, Latitude, Time, Accident_Severity, Day_of_Week) %>%
+    transmute(
+      Longitude,
+      Latitude,
+      Time,
+      Severity = translSeverity(Accident_Severity),
+      Weekday = translWeekday(Day_of_Week)
+    )
+  changeSubset <- reactive({
+    filter(ds, Weekday == input$weekday, Severity == input$type)
+  })
+  output$map <- renderLeaflet({
+    ds2 <- changeSubset()
+    leaflet() %>%
+      addTiles() %>%
+      addCircleMarkers(ds2$Longitude,
+                       ds2$Latitude,
+                       weight = 1,
+                       radius = 2) %>%
+      fitBounds(-5, 52, 1.5, 58)
+  })
+  
+  output$histogram <- renderPlot({
+    ds2 <- changeSubset()
+    ggplot(data.frame(time = as.numeric(ds2$Time) / 60), aes(time)) +
+      geom_histogram(bins = 24) +
+      xlim(c(0, 24)) +
+      ggtitle(paste("Number of",input$type,"accidents on",input$weekday))
+  })
+})
---
+++ DevelopingDataProducts/ddp_courseProject/ui.R
@@ -0,0 +1,60 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("Road accidents in the UK"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("type", "Severity of accident", c("Fatal", "Serious", "Slight")),
+      radioButtons(
+        "weekday",
+        "Day of the week",
+        c(
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday"
+        )
+      )
+    ),
+    # Show a map
+    mainPanel(tabsetPanel(
+      tabPanel(
+        "help",
+        h3("Instructions"),
+        p(
+          "The application uses data about road accidents for the year 2015 from the dataset Road Safety Data provided by Brittish ministerial Department For Transport"
+        ),
+        a(
+          href = "https://data.gov.uk/dataset/road-accidents-safety-data",
+          "https://data.gov.uk/dataset/road-accidents-safety-data"
+        ),
+        p(
+          "To use the app you have to select the weekday of interest and group of accidents based on the accident severity from the sidebar.
+          Then if you select the tab labelled histogram, you should see a histogram of accident frequencies for every hour. If you select the tab map, you will see a map indicating where the accidents happened."
+        )
+        ),
+      tabPanel("map", leafletOutput("map")),
+      tabPanel("histogram", plotOutput("histogram"))
+      
+      
+        ))
+)
+))
